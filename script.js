78649e0af6cb05681bbab13e7bebd7845fe06929
okamoto <okamotoharu@icloud.com>
2026-05-01 12:32:39 +0900

    大吉だけ文字赤色

--- script.js
+++ script.js
@@ -14,7 +14,15 @@ const result = document.getElementById("result");
 button.addEventListener("click", function () {
     //ランダムな数字を作る
     const randomNumber = Math.floor(Math.random() * fortunes.length);
-
+    const resultText = fortunes[(randomNumber)];
+    
     //結果を表示
     result.textContent = fortunes[(randomNumber)];
+
+    if(resultText.includes("大吉")){
+       result.style.color = "red";
+    }else{
+        result.style.color = "black";
+    }
+    
     });
